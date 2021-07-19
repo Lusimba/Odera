0b0d213c74412f380497c85103e38c81bbda2034
lusimba <newville92@gmail.com>
2021-07-19 16:02:42 +0300

    changing logo to png

--- components/Layout.js
+++ components/Layout.js
@@ -6,7 +6,7 @@ function Layout({children}) {
         <div className="container-fluid">
             <Header />
             <div className="text-center fixed-top bg-white">
-                <div className="">Call us on +254706722744</div>
+                <div className="">Call us on <a href="tel:254706722744">+254706722744</a></div>
             </div>
             <main>
                 {children}
